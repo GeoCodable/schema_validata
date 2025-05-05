2c14a93d9ee62c842883f7797a0875cf427b9b54
AHampton NTIA <115647832+ahamptonTIA@users.noreply.github.com>
2025-05-05 14:11:40 -0500

    Update setup.py
    Signed-off-by: AHampton NTIA <115647832+ahamptonTIA@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setuptools.setup(
     install_requires=[
         'pyspark>=3.4.1',
         'pandas>=1.4.4',
-        'numpy>=1.21',  
+        # 'numpy>=1.21',  
         'openpyxl>=3.0',
         'sqlparse>=0.4',
         'sql_metadata>=2.0'
